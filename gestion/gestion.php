c8f3e950d1cb36c32fa9ff06641571587c52792a
Numa42 <mizuki42m@gmail.com>
2024-04-18 14:57:02 +0200

    Création du dossier privé gestion

--- gestion/gestion.php
+++ gestion/gestion.php
@@ -16,62 +16,62 @@ if(!isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <title>La Grange à Légumes BIO</title>
-    <link rel="icon" href="assets/image/icon.png">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/gestion.css">
+    <link rel="icon" href="../assets/image/icon.png">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/gestion.css">
 </head>
 <body>
 <header>
-    <a href="index.html">
-        <img src="assets/image/logo.png">
+    <a href="../index.html">
+        <img src="../assets/image/logo.png">
     </a>
     <section class="title">
         <h1>LA GRANGE À LÉGUMES </h1><h1 class="bio">BIO</h1>
     </section>
     <section id="right">
-        <img id="menuIcon" src="assets/image/menuIcon.png" onclick="toggleStatus()">
-        <img id="menuClose" src="assets/image/menuClose.png" onclick="toggleStatus()">
+        <img id="menuIcon" src="../assets/image/menuIcon.png" onclick="toggleStatus()">
+        <img id="menuClose" src="../assets/image/menuClose.png" onclick="toggleStatus()">
         <nav id="nav">
             <section id="menu0">
-                <a id="acceuil" href="index.html"><p class="pMenu">Acceuil</p></a>
+                <a id="acceuil" href="../index.html"><p class="pMenu">Acceuil</p></a>
             </section>
             <section id="menu1">
                 <p id="entreprise" class="pMenu">L'entreprise</p>
                 <ul id="menuEntreprise" class="menu">
-                    <a href="presentation.html"><li>Présentation</li></a>
-                    <a href="partenaires.html"><li>Partenaires</li></a>
-                    <a href="contact.html"><li>Contact</li></a>
+                    <a href="../presentation.html"><li>Présentation</li></a>
+                    <a href="../partenaires.html"><li>Partenaires</li></a>
+                    <a href="../contact.html"><li>Contact</li></a>
                 </ul>
             </section>
             <section id="menu2">
                 <p id="vente" class="pMenu">Paniers</p>
                 <ul id="menuVente" class="menu">
-                    <a href="formules.html"><li>Formules</li></a>
-                    <a href="conditionsVente.html"><li>Conditions de ventes</li></a>
-                    <a href="tarifs.php"><li>Tarifs légumes</li></a>
+                    <a href="../formules.html"><li>Formules</li></a>
+                    <a href="../conditionsVente.html"><li>Conditions de ventes</li></a>
+                    <a href="../tarifs.php"><li>Tarifs légumes</li></a>
                 </ul>
             </section>
         </nav>
-        <img src="assets/image/logoAB.jpg" id="ab">
+        <img src="../assets/image/logoAB.jpg" id="ab">
     </section>
 </header>
 <main>
     <h2>Pannel de <?php echo $_SESSION['user'] ?></h2>
     <section id="pannel">
         <a href="gestionProduits.php">
-            <img src="assets/image/icon/legumesBold.svg" height="60" width="60" />
+            <img src="../assets/image/icon/legumesBold.svg" height="60" width="60" />
             <h3>Gestion produits</h3>
         </a>
         <a href="gestionFactures.php">
-            <img src="assets/image/icon/facture.svg" height="60" width="60" />
+            <img src="../assets/image/icon/facture.svg" height="60" width="60" />
             <h3>Gestion factures</h3>
         </a>
         <a href="gestionClient.php">
-            <img src="assets/image/icon/personCircle.svg" height="60" width="60" />
+            <img src="../assets/image/icon/personCircle.svg" height="60" width="60" />
             <h3>Gestion clients</h3>
         </a>
         <a href="gestionActus.php">
-            <img src="assets/image/icon/actu.svg" height="60" width="60" />
+            <img src="../assets/image/icon/actu.svg" height="60" width="60" />
             <h3>Actualités</h3>
         </a>
     </section>
@@ -81,5 +81,5 @@ if(!isset($_SESSION['user'])) {
     <p>Ce site web à été réalisé par Numa Verpillon</p>
 </footer>
 </body>
-<script src="js/base.js"></script>
+<script src="../js/base.js"></script>
 </html>
--- gestion/gestionProduits.php
+++ gestion/gestionProduits.php
@@ -12,44 +12,44 @@ $produitDAO = new ProduitDAO(MaBD::getInstance());
 <head>
     <meta charset="UTF-8">
     <title>La Grange à Légumes BIO</title>
-    <link rel="icon" href="assets/image/icon.png">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/tarifs.css">
-    <link rel="stylesheet" href="css/gestionProduits.css">
+    <link rel="icon" href="../assets/image/icon.png">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/tarifs.css">
+    <link rel="stylesheet" href="../css/gestionProduits.css">
 </head>
 <body>
 <header>
-    <a href="index.html">
-        <img src="assets/image/logo.png">
+    <a href="../index.html">
+        <img src="../assets/image/logo.png">
     </a>
     <section class="title">
         <h1>LA GRANGE À LÉGUMES </h1><h1 class="bio">BIO</h1>
     </section>
     <section id="right">
-        <img id="menuIcon" src="assets/image/menuIcon.png" onclick="toggleStatus()">
-        <img id="menuClose" src="assets/image/menuClose.png" onclick="toggleStatus()">
+        <img id="menuIcon" src="../assets/image/menuIcon.png" onclick="toggleStatus()">
+        <img id="menuClose" src="../assets/image/menuClose.png" onclick="toggleStatus()">
         <nav id="nav">
             <section id="menu0">
-                <a id="acceuil" href="index.html"><p class="pMenu">Acceuil</p></a>
+                <a id="acceuil" href="../index.html"><p class="pMenu">Acceuil</p></a>
             </section>
             <section id="menu1">
                 <p id="entreprise" class="pMenu">L'entreprise</p>
                 <ul id="menuEntreprise" class="menu">
-                    <a href="presentation.html"><li>Présentation</li></a>
-                    <a href="partenaires.html"><li>Partenaires</li></a>
-                    <a href="contact.html"><li>Contact</li></a>
+                    <a href="../presentation.html"><li>Présentation</li></a>
+                    <a href="../partenaires.html"><li>Partenaires</li></a>
+                    <a href="../contact.html"><li>Contact</li></a>
                 </ul>
             </section>
             <section id="menu2">
                 <p id="vente" class="pMenu">Paniers</p>
                 <ul id="menuVente" class="menu">
-                    <a href="formules.html"><li>Formules</li></a>
-                    <a href="conditionsVente.html"><li>Conditions de ventes</li></a>
-                    <a href="tarifs.php"><li>Tarifs légumes</li></a>
+                    <a href="../formules.html"><li>Formules</li></a>
+                    <a href="../conditionsVente.html"><li>Conditions de ventes</li></a>
+                    <a href="../tarifs.php"><li>Tarifs légumes</li></a>
                 </ul>
             </section>
         </nav>
-        <img src="assets/image/logoAB.jpg" id="ab">
+        <img src="../assets/image/logoAB.jpg" id="ab">
     </section>
 </header>
 <main>
@@ -73,5 +73,5 @@ $produitDAO = new ProduitDAO(MaBD::getInstance());
     <p>Ce site web à été réalisé par Numa Verpillon</p>
 </footer>
 </body>
-<script src="js/base.js"></script>
+<script src="../js/base.js"></script>
 </html>
