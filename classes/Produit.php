9f9cb2c0fb257724c5e1e93ea0e40426ac4ebaac
Numa42 <mizuki42m@gmail.com>
2024-04-24 11:35:45 +0200

    Forulaire gestion produits

--- classes/Produit.php
+++ classes/Produit.php
@@ -31,19 +31,19 @@ class Produit
         $producteurDAO = new ProducteurDAO(MaBD::getInstance());
 
         echo "<form method='post'>
-            <p class='form'>
-            <p>$this->id</p>
-            <section class='text'>
-                <input name='Code' type='text' value='$this->code' />
-                <input name='Nom' type='text' value='$this->nom' />
-                <input name='Type' type='text' value='$this->type' />
-                <input name='Fournisseur' type='text' value='' />
-                <input name='Prix' type='text' value='$this->prix' />
-                <input name='Unité' type='text' value='$this->unite' />
-            </section>
-            <input type='image' name='edit[]' class='btn-content' src='/assets/image/icon/edit.svg'>
-            <input type='image' name='remove[]' class='btn-content' src='../assets/image/icon/remove.svg'>
-            </p>
+                <section class='form'>
+                    <p>$this->id</p>
+                    <section class='text'>
+                        <input name='Code' type='text' value='$this->code' />
+                        <input name='Nom' type='text' value='$this->nom' />
+                        <input name='Type' type='text' value='$this->type' />
+                        <input name='Fournisseur' type='text' value='' />
+                        <input name='Prix' type='text' value='$this->prix' />
+                        <input name='Unité' type='text' value='$this->unite' />
+                    </section>
+                    <input type='image' name='edit[]' class='btn-content' src='/assets/image/icon/edit.svg'>
+                    <input type='image' name='remove[]' class='btn-content' src='../assets/image/icon/remove.svg'>
+                </section>
         </form>";
 //        $producteurDAO->getOne($this->source)->nom
     }
