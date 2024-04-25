0d5376ad2aa855b8fa9673e6d21961b273de20bf
Numa42 <mizuki42m@gmail.com>
2024-04-25 10:29:34 +0200

    Edition des produits debug

--- classes/ProduitDAO.php
+++ classes/ProduitDAO.php
@@ -25,7 +25,7 @@ class ProduitDAO extends DAO
 
     public function save(object $obj): int
     {
-        $stmt = $this->pdo->query("UPDATE Produit SET code_balance=?, nom_produit=?, type_produit=?, source=?, prix=?, unite=? WHERE id_produit=?");
+        $stmt = $this->pdo->prepare("UPDATE Produit SET code_balance=?, nom_produit=?, type_produit=?, source=?, prix=?, unite=? WHERE id_produit=?");
         $res = $stmt->execute(array($obj->getAll()));
         return $res;
     }
