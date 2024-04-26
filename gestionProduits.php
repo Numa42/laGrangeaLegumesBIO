56786eb014852b2821162bf8fbbf6256b5fd9066
Numa42 <mizuki42m@gmail.com>
2024-04-26 15:00:10 +0200

    Edition des produits debug

--- gestionProduits.php
+++ gestionProduits.php
@@ -80,13 +80,14 @@ $produitDAO = new ProduitDAO(MaBD::getInstance());
             $res = $produitDAO->edit($produit);
             echo "<p>".$res."</p>";
         }
-        if (isset($_POST['remove'])) {
-            echo "<p>COUBEH</p>";
-        }
+
         ?>
     </section>
     <section class="main">
         <?php
+        if (isset($_POST['remove'])) {
+            echo "<p>COUBEH</p>";
+        }
         echo "<section class='form'>
                     <p class='id'></p>
                     <section class='text'>
